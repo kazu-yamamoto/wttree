b3e18ce6b54699705e011f519fa516555eb6354f
Kazu Yamamoto <kazu@iij.ad.jp>
2010-12-01 18:49:17 +0900

    adding union.

--- slib/test.scm
+++ slib/test.scm
@@ -40,6 +40,16 @@
 (define (from-alist al)
   (alist->wt-tree number-wt-type al))
 
+(define (to-list tree)
+  (wt-tree/fold (lambda (k v l) (cons k l)) '() tree))
+
+(define (uniq x)
+  (define (func y z)
+    (if (and (not (null? z)) (equal? y (car z)))
+	z
+	(cons y z)))
+  (fold-right func () x))
+
 ;;
 ;; Engine
 ;;
@@ -75,8 +85,26 @@
 (define (prop-alist->wt-tree alst)
   (wt-tree/valid? (from-alist alst)))
 
+(define (prop-wt-tree/add alst k v)
+  (wt-tree/valid? (wt-tree/add (from-alist alst) k v)))
+
+(define (prop-wt-tree/lookup alst k v)
+  (let ((tree (wt-tree/add (from-alist alst) k v)))
+    (eq? (wt-tree/lookup tree k #f) v)))
+
 (define (prop-wt-tree/delete-min alst)
   (wt-tree/valid? (wt-tree/delete-min (from-alist alst))))
+
+(define (prop-wt-tree/union alst1 alst2)
+  (let* ((l1 (uniq (sort (map car alst1))))
+	 (l2 (uniq (sort (map car alst2))))
+	 (model (sort (lset-union eq? l1 l2)))
+	 (t1 (from-alist alst1))
+	 (t2 (from-alist alst2))
+	 (this (sort (to-list (wt-tree/union t1 t2)))))
+    (equal? model this)))
+
+;; wt-tree/intersection, wt-tree/difference
 
 ;;
 ;; test db
@@ -85,13 +113,16 @@
 (define test-alist
   (list
    (list "alist->wt-tree" prop-alist->wt-tree 'alist)
-   (list "wt-tree/delete-min" prop-wt-tree/delete-min 'alist)))
+   (list "prop-wt-tree/add" prop-wt-tree/add 'alist 'int 'int)
+   (list "prop-wt-tree/lookup" prop-wt-tree/lookup 'alist 'int 'int)
+   (list "wt-tree/delete-min" prop-wt-tree/delete-min 'alist)
+   (list "prop-wt-tree/union" prop-wt-tree/union 'alist 'alist)))
 
 ;;
 ;; main
 ;;
 
-(define number-of-tests 1000)
+(define number-of-tests 100)
 
 (dolist (prop test-alist)
   (let ((tag (car prop))
