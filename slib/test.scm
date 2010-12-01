feda17d5d1128b961eea58e6908d14d983183e70
Kazu Yamamoto <kazu@iij.ad.jp>
2010-12-01 16:11:38 +0900

    scratch of auto testing.

--- slib/test.scm
+++ slib/test.scm
@@ -1,13 +1,29 @@
 (load "./wttree.scm")
+(use srfi-1)
+(use srfi-27)
+(random-source-randomize! default-random-source)
 
-(define x (alist->wt-tree number-wt-type '((-3 . -3)
-					   (-6 . -6)
-					   (3 . 3)
-					   (1 . 1)
-					   (7 . 7)
-					   (-2 . -2)
-					   (5 . 5))))
+(define x (alist->wt-tree number-wt-type '((-3)
+					   (-6)
+					   (3)
+					   (1)
+					   (7)
+					   (-2)
+					   (5))))
 ;;(wt-tree/valid? x)
 ;;(wt-tree/valid? (wt-tree/delete-min x))
 
+(define (uniq x)
+  (define (func y z)
+    (if (and (not (null? z)) (equal? y (car z)))
+	z
+	(cons y z)))
+  (fold-right func () x))
 
+(define (random-list n)
+  (list-tabulate n
+		 (lambda (dummy)
+		   (random-integer n))))
+
+(print (wt-tree/valid? x))
+(print (wt-tree/valid? (wt-tree/delete-min x)))
